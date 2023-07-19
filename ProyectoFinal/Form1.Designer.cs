bfbbbca371ff549f7cb41ba55680e745ed3b967c
aleja <aleja@ALEJANDROMG>
2023-07-19 09:39:21 -0500

    CambioLogin

--- ProyectoFinal/Form1.Designer.cs
+++ ProyectoFinal/Form1.Designer.cs
@@ -29,110 +29,147 @@ namespace ProyectoFinal
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.btnRedirigir = new System.Windows.Forms.Button();
-            this.tabControl1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.radAdministrador = new System.Windows.Forms.RadioButton();
+            this.radEmpleado = new System.Windows.Forms.RadioButton();
+            this.btnIniciarSesion = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // radAdministrador
             // 
-            this.button1.Location = new System.Drawing.Point(336, 150);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.radAdministrador.AutoSize = true;
+            this.radAdministrador.Location = new System.Drawing.Point(624, 317);
+            this.radAdministrador.Name = "radAdministrador";
+            this.radAdministrador.Size = new System.Drawing.Size(116, 21);
+            this.radAdministrador.TabIndex = 20;
+            this.radAdministrador.Text = "Administrador";
+            this.radAdministrador.UseVisualStyleBackColor = true;
             // 
-            // tabControl1
+            // radEmpleado
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(518, 84);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(200, 100);
-            this.tabControl1.TabIndex = 1;
+            this.radEmpleado.AutoSize = true;
+            this.radEmpleado.Checked = true;
+            this.radEmpleado.Location = new System.Drawing.Point(485, 317);
+            this.radEmpleado.Name = "radEmpleado";
+            this.radEmpleado.Size = new System.Drawing.Size(92, 21);
+            this.radEmpleado.TabIndex = 19;
+            this.radEmpleado.TabStop = true;
+            this.radEmpleado.Text = "Empleado";
+            this.radEmpleado.UseVisualStyleBackColor = true;
             // 
-            // tabPage1
+            // btnIniciarSesion
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
-            this.tabPage1.Size = new System.Drawing.Size(192, 74);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this.btnIniciarSesion.Location = new System.Drawing.Point(485, 361);
+            this.btnIniciarSesion.Name = "btnIniciarSesion";
+            this.btnIniciarSesion.Size = new System.Drawing.Size(219, 48);
+            this.btnIniciarSesion.TabIndex = 18;
+            this.btnIniciarSesion.Text = "Iniciar Sesión";
+            this.btnIniciarSesion.UseVisualStyleBackColor = true;
+            this.btnIniciarSesion.Click += new System.EventHandler(this.btnIniciarSesion_Click);
             // 
-            // tabPage2
+            // pictureBox1
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
-            this.tabPage2.Size = new System.Drawing.Size(192, 74);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(415, 446);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
             // 
-            // numericUpDown1
+            // txtPassword
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(215, 60);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
-            this.numericUpDown1.TabIndex = 2;
+            this.txtPassword.Location = new System.Drawing.Point(485, 267);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(219, 22);
+            this.txtPassword.TabIndex = 16;
             // 
-            // dateTimePicker1
+            // txtUsername
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(260, 254);
-            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(151, 20);
-            this.dateTimePicker1.TabIndex = 3;
+            this.txtUsername.Location = new System.Drawing.Point(485, 173);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(219, 22);
+            this.txtUsername.TabIndex = 15;
             // 
-            // btnRedirigir
+            // label3
             // 
-            this.btnRedirigir.Location = new System.Drawing.Point(47, 132);
-            this.btnRedirigir.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.btnRedirigir.Name = "btnRedirigir";
-            this.btnRedirigir.Size = new System.Drawing.Size(94, 59);
-            this.btnRedirigir.TabIndex = 4;
-            this.btnRedirigir.Text = "Test Altas y Bajas Libros";
-            this.btnRedirigir.UseVisualStyleBackColor = true;
-            this.btnRedirigir.Click += new System.EventHandler(this.btnRedirigir_Click);
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(481, 214);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 24);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Contraseña";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(481, 125);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 24);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Usuario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(480, 68);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(224, 29);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Inicie sesión aquí!";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.btnRedirigir);
-            this.Controls.Add(this.dateTimePicker1);
-            this.Controls.Add(this.numericUpDown1);
-            this.Controls.Add(this.tabControl1);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(780, 486);
+            this.Controls.Add(this.radAdministrador);
+            this.Controls.Add(this.radEmpleado);
+            this.Controls.Add(this.btnIniciarSesion);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.tabControl1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabPage tabPage2;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
-        private System.Windows.Forms.Button btnRedirigir;
+        private System.Windows.Forms.RadioButton radAdministrador;
+        private System.Windows.Forms.RadioButton radEmpleado;
+        private System.Windows.Forms.Button btnIniciarSesion;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- ProyectoFinal/Form1.cs
+++ ProyectoFinal/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -31,7 +32,75 @@ namespace ProyectoFinal
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            radEmpleado.Checked = true;
+        }
 
+        private void btnIniciarSesion_Click(object sender, EventArgs e)
+        {
+            //Agregar campo Contraseña a la tabla usuario
+            string usuario = txtUsername.Text;
+            string contraseña = txtPassword.Text;
+
+
+            if (string.IsNullOrEmpty(usuario) || string.IsNullOrEmpty(contraseña))
+            {
+                MessageBox.Show("Ingresa un nombre de usuario y contraseña válidos.");
+                return;
+            }
+            if (radAdministrador.Checked)
+            {
+                //cambiar ALEJANDROMG\\SQLEXPRESS01 por el nombre de su servidor
+                string connectionString = "Data Source = ALEJANDROMG\\SQLEXPRESS01; Initial Catalog = Biblioteca; User ID=Administrador;Password=Administrador; "; // Reemplaza esto con tu cadena de conexión
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    string query = "SELECT COUNT(*) FROM Usuarios WHERE Nombre = @Usuario AND Contraseña = @Contraseña AND ID_Usuario = 1";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Usuario", usuario);
+                        command.Parameters.AddWithValue("@Contraseña", contraseña);
+
+                        connection.Open();
+                        int count = (int)command.ExecuteScalar();
+
+                        if (count > 0)
+                        {
+                            MessageBox.Show("hola administrador");//Formulario de los administradores
+                        }
+                        else
+                        {
+                            MessageBox.Show("Credenciales incorrectas o usuario no es empleado.");
+                        }
+                    }
+                }
+            }
+            if (radEmpleado.Checked)
+            {
+                //cambiar ALEJANDROMG\\SQLEXPRESS01 por el nombre de su servidor
+                string connectionString = "Data Source = ALEJANDROMG\\SQLEXPRESS01; Initial Catalog = Biblioteca; User ID=Empleado;Password=Empleado; ";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    string query = "SELECT COUNT(*) FROM Usuarios WHERE Nombre = @Usuario AND Contraseña = @Contraseña AND ID_Usuario = 2"; // Verifica que el ID del usuario sea 2 (empleados)
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Usuario", usuario);
+                        command.Parameters.AddWithValue("@Contraseña", contraseña);
+
+                        connection.Open();
+                        int count = (int)command.ExecuteScalar();
+
+                        if (count > 0)
+                        {
+                            MessageBox.Show("¡Bienvenido, Empleado!");//Formulario del empleado
+                        }
+                        else
+                        {
+                            MessageBox.Show("Credenciales incorrectas o usuario no es empleado.");
+                        }
+                    }
+                }
+            }
         }
     }
 }
